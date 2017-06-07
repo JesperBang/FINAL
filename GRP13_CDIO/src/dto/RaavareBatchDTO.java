755713ac3442a12a83b92f884d3cffce310c4fb9
ChristopherChytraeus <Christopherchytraeus@gmail.com>
2017-06-07 15:22:21 +0200

    MySQLDAO/GET/SET: RaavareBatchDTO, RaavareDTO

--- GRP13_CDIO/src/dto/RaavareBatchDTO.java
+++ GRP13_CDIO/src/dto/RaavareBatchDTO.java
@@ -4,10 +4,31 @@ public class RaavareBatchDTO {
 	
 	/** raavare batch id i området 1-99999999. Vælges af brugerne */  
 	int rbId;                     
+	public int getRbId() {
+		return rbId;
+	}
+	public void setRbId(int rbId) {
+		this.rbId = rbId;
+	}
+	
+	
 	/** raavare id i området 1-99999999 vælges af brugerne */
-	int raavareId;             
+	int raavareId;          
+	public int getRaavareId() {
+		return raavareId;
+	}
+	public void setRaavareId(int raavareId) {
+		this.raavareId = raavareId;
+	}
+	
 	/** mængde på lager */
 	double maengde;
+	public double getMaengde() {
+		return maengde;
+	}
+	public void setMaengde(double maengde) {
+		this.maengde = maengde;
+	}
 
 
 }
--- GRP13_CDIO/src/dto/RaavareDTO.java
+++ GRP13_CDIO/src/dto/RaavareDTO.java
@@ -2,12 +2,26 @@ package dto;
 
 public class RaavareDTO {
 	
+	
 	/** raavare id i området 1-99999999 vælges af brugerne */
-	int raavareId;                     
+	int raavareId; 
+	
+	public int getRaavareId() {return raavareId;}
+	public void setRaavareId(int raavareId) {this.raavareId = raavareId;}
+	
+	
 	/** min. 2 max. 20 karakterer */
-	String raavareNavn;                
+	String raavareNavn; 
+	
+	public String getRaavareNavn() {return raavareNavn;}
+	public void setRaavareNavn(String raavareNavn) {this.raavareNavn = raavareNavn;}
+	
+	
 	/** min. 2 max. 20 karakterer */
 	String leverandoer;
+	
+	public String getLeverandoer() {return leverandoer;}
+	public void setLeverandoer(String leverandoer) {this.leverandoer = leverandoer;}
 
 
 }
