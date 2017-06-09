857f2c9a96e8f70b4a49553b9e5da2d7b99f9206
Thomas <Thomas@Tklorentzen-PC.eduroam.wireless.dtu.dk>
2017-06-09 12:22:11 +0200

    Nyt til JS filer for de forskellige

--- GRP13_CDIO/WebContent/js/raavare.js
+++ GRP13_CDIO/WebContent/js/raavare.js
@@ -4,11 +4,10 @@
 
 $(document).ready(function() {
 	
-	var allUsers;
+	var allRaavarer;
 	
 	//Load raavare creation by request
 	document.getElementbyID("ORV").addEventListener("click",function(){
-		alert("SSSS");
 		$("#createraavare").show();
 		return false;
 	
@@ -25,8 +24,91 @@ $(document).ready(function() {
 		$("#table").hide();
 		$("#ShowRaavare").show();
 		return false;
+		
+	// ajax request
+	$.ajax({
+		url: "http://localhost:8080/GRP13_CDIO/rest2/userservice/raavare",
+		method: "POST",
+		
+		//success function
+		success: function(data){
+			allRaavarer = data;
+			console.log(data);
+			//Parse JSON from ajax request to html table
+			//ini vars for table row
+			var tr;
+			$("#raavaretable tr").remove();
+			$("#raavaretable th").remove();
+		
+			//Append table row with descriptions
+			$('<tr>').append(
+					$('<th>').text("raavareID"),
+					$('<th>').text("raavareName"),
+					$('<th>').text("leverandoer")
+			).appendTo("#raavaretable");
+			
+			//Loop through raavare and append them to the table in html
+			$.each(allUsers, function(i, item) {
+				$('<tr>').append(
+						$('<td>').text(item.raavareID),
+						$('<td>').text(item.raavareName),
+						$('<td>').text(item.leverandoer)
+				).appendTo('#raavaretable');
+			});
+			
+		},
+		
+		//error function
+		error: function(error){
+			alert("Error, sorry! :(");
+		},
+
+	});
+		return false;
+	});
+	document.getElementById("Forfattere").addEventListener("click",function myFunction() {
+	    var popup = document.getElementById("myPopup");
+	    popup.classList.toggle("show");
+	    $("#popupID").show();
+	    
+	});
+	
+	
+		//Create User Submit Button
+		$("#CreateUserForm").submit( function() {               
+   
+			event.preventDefault();
+			
+			
+			var data = $('#CreateUserForm').serializeObject();
+
+			console.log(data);
+			debugger;
+			$.ajax({
+				url: "http://localhost:8080/GRP13_CDIO/rest2/raavareservice",
+				data: JSON.stringify(data),
+				contentType: "application/json",
+				method: 'POST',
+				success: function(resp){
+					console.log(data);
+					console.log('This is the Success method')
+					console.log(resp)
+					document.getElementById("CreateUserForm").reset();
+
+				},
+				error: function(resp){
+					console.log(data);
+					console.log('This is the ERROR method')
+					console.log(resp)
+				}
+			});
+		   
+			return false;
+
+	});	
+		
 	});
 
 	
 	
-});
+
--- GRP13_CDIO/WebContent/js/raavarebatch.js
+++ GRP13_CDIO/WebContent/js/raavarebatch.js
@@ -4,19 +4,100 @@
 
 $(document).ready(function() {
 	
-	var allUsers;
-	
+	var allRBs;
 	//Load raavarebatch Showing by request
 	document.getElementbyID("RBO").addEventListener("click",function(){
+		alert("somthing");
 		$("#CreateRaavarebatch").show();
 		return false;
 	
 	});
 	//load raavarebatch showing by request
 	document.getElementbyID("RBV").addEventListener("click",function(){
-		$("#ShowRaavarebatch").show();
+		$("#ShowRaavareBatch").show();
 		return false;
 	});
 	
+	// ajax request
+	$.ajax({
+		url: "http://localhost:8080/GRP13_CDIO/rest2/raavarebatchservice",
+		method: "GET",
+		
+		//success function
+		success: function(data){
+			allRBs = data;
+			console.log(data);
+			//Parse JSON from ajax request to html table
+			//ini vars for table row
+			var tr;
+			$("#raavarebatchtable tr").remove();
+			$("#raavarebatchtable th").remove();
+		
+			//Append table row with descriptions
+			$('<tr>').append(
+					$('<th>').text("rbID"),
+					$('<th>').text("raavareID"),
+					$('<th>').text("maengde")
+			).appendTo("#raavarebatchtable");
+			
+			//Loop through raavare and append them to the table in html
+			$.each(allRBs, function(i, item) {
+				$('<tr>').append(
+						$('<td>').text(item.rbID),
+						$('<td>').text(item.raavareID),
+						$('<td>').text(item.maengde)
+				).appendTo('#raavarebatchtable');
+			});
+			
+		},
+		
+		//error function
+		error: function(error){
+			alert("Error, sorry! :(");
+		},
+
+	});
+		return false;
+	});
+	document.getElementById("Forfattere").addEventListener("click",function myFunction() {
+	    var popup = document.getElementById("myPopup");
+	    popup.classList.toggle("show");
+	    $("#popupID").show();
+	    
+	});
 	
-});
+	
+		//Create User Submit Button
+		$("#CreateUserForm").submit( function() {               
+   
+			event.preventDefault();
+			
+			
+			var data = $('#CreateUserForm').serializeObject();
+
+			console.log(data);
+			debugger;
+			$.ajax({
+				url: "http://localhost:8080/GRP13_CDIO/rest2/raavarebatchservice/raavarebatch",
+				data: JSON.stringify(data),
+				contentType: "application/json",
+				method: 'POST',
+				success: function(resp){
+					console.log(data);
+					console.log('This is the Success method')
+					console.log(resp)
+					document.getElementById("CreateUserForm").reset();
+
+				},
+				error: function(resp){
+					console.log(data);
+					console.log('This is the ERROR method')
+					console.log(resp)
+				}
+			});
+		   
+			return false;
+
+		
+	});
+
