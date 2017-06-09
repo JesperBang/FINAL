ceb51422c75bdd2f698262abfdf02b2c42a25e81
ChristopherChytraeus <Christopherchytraeus@gmail.com>
2017-06-09 12:12:02 +0200

    Receptadmin Vis/Opret

--- GRP13_CDIO/WebContent/js/receptadmin.js
+++ GRP13_CDIO/WebContent/js/receptadmin.js
@@ -6,17 +6,23 @@ $(document).ready(function() {
 
 
 	var allPrescriptions;
+	
+	document.getElementById("OpretReceptSM").addEventListener("click",function() {
+		$("#table").hide();
+		$("#createprescript").show();
+		return false;
+		
+	});
 
 	// Load prescriptions on Vis Recepter page
-	document.getElementById("Receptadmin").addEventListener("click",function() {
+	document.getElementById("VisReceptSM").addEventListener("click",function() {
 
 		//visuals
-		$("#table").show();
-
+		$("#receptable").show();
 
 		//ajax request
 		$.ajax({
-			url: "http://localhost:8080/GRP13_CDIO/rest2/userservice/recept",
+			url: "http://localhost:8080/GRP13_CDIO/rest2/receptservice/recept",
 			method: "GET",
 
 			//success function
@@ -27,9 +33,70 @@ $(document).ready(function() {
 				//ini vars for table row
 				var tr;
 
+				//clearing old table rows and table heads
+				$("#recepttable tr").remove();
+				$("#recepttable th").remove();
+
+				//Append table row with descriptions
+				$('<tr>').append(
+						$('<th>').text("Recept ID"),
+						$('<th>').text("Navn"),
+						$('<th>').text("Receptkomponenter")
+				).appendTo("#recepttable");
+
+				//Loop through prescriptions and append them to the table in html
+				$.each(allPrescriptions, function(i, item) {
+					$('<tr>').append(
+							$('<td>').text(item.receptId),
+							$('<td>').text(item.receptNavn),
+							$('<td>').text(item.receptId)
+					).appendTo('#recepttable');
+				});
+
 			}
 		});
 
 	});
 	
+	
+
+	//Create Prescription Button
+	$("#CreatePrescription").submit( function() {               
+
+		event.preventDefault();
+
+
+		var data = $('#CreatePrescription').serializeObject();
+
+		console.log(data);
+		debugger;
+		$.ajax({
+			url: "http://localhost:8080/GRP13_CDIO/rest2/receptservice/create/recept",
+			data: JSON.stringify(data),
+			contentType: "application/json",
+			method: 'POST',
+			success: function(resp){
+				console.log(data);
+				console.log('This is the Success method')
+				console.log(resp)
+				document.getElementById("CreatePrescription").reset();
+				console.log("CPForm has been cleared")
+
+				//Goes back to menu
+				$('#usradmin').show();
+				$('#receptable').hide();
+
+			},
+			error: function(resp){
+				console.log(data);
+				console.log('This is the ERROR method')
+				console.log(resp)
+			}
+		});
+
+		return false;
+
+	});
+
 });
+	
