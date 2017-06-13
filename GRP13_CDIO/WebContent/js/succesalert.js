7a91b6ced4f2b3f65f4b41d3d683e89420fbcb51
Johnny <Johnny@Johnny.eduroam.wireless.dtu.dk>
2017-06-13 15:03:43 +0200

    Succesalert almost added

---
+++ GRP13_CDIO/WebContent/js/succesalert.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+	document.getElementById("CUbtn").addEventListener("click",function myFunction() {
+		if ($("#CreateUserForm").submit.is('true')) {
+			var succes = document.getElementById('succes').style.display = 'block';
+		}
+	});
+	
+});
